30fd9e2b9b1b3b8e2891c4ff75e7a5825a81149e
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-04-18 12:54:24 +0900

    props를 활용한 컴포넌트 통신

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-var localCmp = {
-  template: '<div>this is a local cmp </div>'
-}
-
-Vue.component('my-global-component', {
-  template: '<div>this is a global cmp </div>'
-})
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message : 'This is a paraent component'
-  },
-  components:{
-    'my-local-component' : localCmp
-  }
-})
+  //
+  //
+  // var cmp1 = {
+  //   template : '<div>첫 번째 지역 컴포넌트 : {{cmp1Data}} </div>',
+  //   data: function(){
+  //     return {
+  //       cmp1Data : 100
+  //     }
+  //   }
+  // };
+  //
+  // var cmp2 = {
+  //   template : '<div>두 번째 지역 컴포넌트 : {{cmp2Data}} </div>',
+  //   data: function(){
+  //     return {
+  //       cmp2data : cmp1.data.cmp1Data
+  //     }
+  //   }
+  // };
+  //
+  //
+  // var app = new Vue({
+  //   el: '#app',
+  //   components:{
+  //     'my-component1' : cmp1,
+  //     'my-component2' : cmp2
+  //   }
+  // });
